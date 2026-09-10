a4840e41a968f16b356c9843ec78aad3e07623d0
geovanabsouza <geovanabsouza08@gmail.com>
2026-09-10 09:31:34 -0300

    Etapa 2

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+from collections.abc import Generator
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
@@ -26,3 +28,11 @@ criar_sessao = sessionmaker(bind=mecanismo_banco, autoflush=False, autocommit=Fa
 
 class BaseBanco(DeclarativeBase):
     pass
+
+def obter_sessao_banco() -> Generator[Session, None, None]:
+    sessao_banco = criar_sessao()
+
+    try:
+        yield sessao_banco
+    finally:
+        sessao_banco.close()
